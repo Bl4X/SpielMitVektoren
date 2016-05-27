cce8c9469c244ff2c94fb52a80c712226085a7e4
Niclas Heuser <niclas@nilux>
2016-05-27 23:17:20 +0200

    gitignore Datei angehangen

--- repository.sh
+++
@@ -1,7 +0,0 @@
-echo "# SpielMitVektoren" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Bl4X/SpielMitVektoren.git
-git push -u origin master
-
